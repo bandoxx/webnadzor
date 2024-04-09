cf04ead0e28493252cb0ed2d470e576777517ef1
Radivoje Pupovac <radivoje.pupovac98@gmail.com>
2024-04-09 16:21:55 +0200

    Refactored image service

--- src/Controller/ImageController.php
+++ src/Controller/ImageController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Repository\DeviceDataRepository;
+use App\Service\Image\ImageGenerator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,39 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class ImageController extends AbstractController
 {
 
-    #[Route('/image/{deviceId}/{entry}.png', name: 'app_image_getimage')]
-    public function getImage($deviceId, $entry, DeviceDataRepository $deviceDataRepository)
+    #[Route('/image/t/{deviceId}/{entry}.png', name: 'app_image_getimage')]
+    public function getTImage($deviceId, $entry, ImageGenerator $imageGenerator)
     {
-        $data = $deviceDataRepository->findLastRecordForDeviceId($deviceId, $entry);
-
-        $temperature = $data?->getT($entry);
-
         $response = new StreamedResponse(
-            function () use ($temperature) {
-                $im = @ImageCreateFromPNG(__DIR__ . '/../../public/assets/images/termostat.png');
-                imagealphablending($im, false);
-                imagesavealpha($im, true);
-                $blue = ImageColorAllocate($im, 50, 110, 176);
-                $red = ImageColorAllocate($im, 255, 0, 0);
-
-
-                $rx = 74;
-                $ry = 246;
-                $rWidth = 4;
-
-                if (!is_null($temperature)) {
-                    $r_height = 238-196/90*(40 + $temperature);
-                    $fill_color = $blue;
-                } else {
-                    $r_height = 42;
-                    imagestring($im, 3, 52, 18, 'Greska!', $red);
-                    $fill_color = $red;
-                }
-
-                ImageFilledRectangle($im, $rx, $ry, $rx+$rWidth, $r_height, $fill_color);
-
-                ImagePNG($im);
-                imagedestroy($im);
+            function () use ($imageGenerator, $deviceId, $entry) {
+                $imageGenerator->generateTermometer($deviceId, $entry);
             }
         );
 
@@ -54,4 +27,20 @@ class ImageController extends AbstractController
         return $response;
     }
 
+    #[Route('/image/rh/{deviceId}/{entry}.png', name: 'app_image_rh')]
+    public function getRHImage($deviceId, $entry, ImageGenerator $imageGenerator)
+    {
+        $response = new StreamedResponse(
+            function () use ($imageGenerator, $deviceId, $entry) {
+                $imageGenerator->generateRelativyHumidity($deviceId, $entry);
+            }
+        );
+
+        $response->headers->set('Content-Type', 'image/png');
+        $response->headers->set('Cache-Control', 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
+        $response->headers->set('Expires', 'Thu, 19 Nov 1981 08:52:00 GMT');
+        $response->setSharedMaxAge(60);
+
+        return $response;
+    }
 }
---
+++ src/Service/Image/ImageGenerator.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Service\Image;
+
+use App\Repository\DeviceDataRepository;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+class ImageGenerator
+{
+
+    private string $publicDir;
+    private array $data = [];
+
+    public function __construct(private DeviceDataRepository $deviceDataRepository, private KernelInterface $kernel)
+    {
+        $this->publicDir = $this->kernel->getProjectDir() . '/public';
+    }
+
+    public function generateTermometer($deviceId, $entry): void
+    {
+        $data = $this->getDeviceData($deviceId, $entry);
+
+        $temperature = $data?->getT($entry);
+
+        $im = @ImageCreateFromPNG($this->publicDir .'/assets/images/termostat.png');
+        imagealphablending($im, false);
+        imagesavealpha($im, true);
+        $blue = ImageColorAllocate($im, 50, 110, 176);
+        $red = ImageColorAllocate($im, 255, 0, 0);
+
+        $rx = 74;
+        $ry = 246;
+        $rWidth = 4;
+
+        if ($temperature) {
+            $rHeight = 238-196/90*(40 + (float) $temperature);
+            $fillColor = $blue;
+        } else {
+            $rHeight = 42;
+            imagestring($im, 3, 52, 18, 'Greska!', $red);
+            $fillColor = $red;
+        }
+
+        ImageFilledRectangle($im, $rx, $ry, $rx+$rWidth, $rHeight, $fillColor);
+
+        ImagePNG($im);
+        ImageDestroy($im);
+    }
+
+    public function generateRelativyHumidity($deviceId, $entry)
+    {
+        $data = $this->getDeviceData($deviceId, $entry);
+
+        $rh = $data?->getRh($entry);
+
+        $im = @ImageCreateFromPNG($this->publicDir . '/assets/images/rh_scale.png');
+        imagealphablending($im, true);
+        imagesavealpha($im, true);
+
+        $im_dial = @ImageCreateFromPNG($this->publicDir . '/assets/images/rh_dial.png');
+        imagealphablending($im_dial, false);
+        imagesavealpha($im_dial, true);
+
+        // create a new image from the sizes on transparent canvas
+        $new = imagecreatetruecolor(imagesx($im_dial), imagesy($im_dial));
+
+        if ($rh > 100) {
+            $rh = 100;
+        } elseif ($rh < 0) {
+            $rh = 0;
+        }
+
+        $dataset = [
+            [0.0, 136],
+            [10, 99],
+            [20, 67.5],
+            [30, 38],
+            [40, 10],
+            [50, -17],
+            [60, -45],
+            [70, -70.5],
+            [80, -96],
+            [90, -117],
+            [100.0, -132]
+        ];
+
+        $n = count($dataset);
+
+        // Calculate Lagrange basis polynomials
+        $polynomials = [];
+        for ($k = 0; $k < $n; $k++) {
+            $polynomials[$k] = 1;
+            foreach ($dataset as $i => $iValue) {
+                if ($i !== $k) {
+                    $polynomials[$k] *= ($rh - $iValue[0]) / ($dataset[$k][0] - $iValue[0]);
+                }
+            }
+        }
+
+        // Calculate interpolated value
+        $rdeg = 0;
+        for ($k = 0; $k < $n; $k++) {
+            $rdeg += $dataset[$k][1] * $polynomials[$k];
+        }
+
+        $transparent = imagecolorallocatealpha($new, 0, 0, 0, 127);
+        $rotate = imagerotate($im_dial, $rdeg, $transparent);
+        imagealphablending($rotate, true);
+        imagesavealpha($rotate, true);
+
+        // get the newest image X and Y
+        $ix = imagesx($rotate);
+        $iy = imagesy($rotate);
+
+        //copy the image to the canvas
+        imagecopy($im, $rotate, 80-$ix/2, 80-$iy/2, 0, 0, $ix, $iy);
+
+        ImagePNG($im);
+        ImageDestroy($im);
+    }
+
+    private function getDeviceData($deviceId, $entry)
+    {
+        $key = sprintf("%s-%s", $deviceId, $entry);
+
+        if (array_key_exists($key, $this->data)) {
+            return $this->data[$key];
+        }
+
+        $this->data[$key] = $this->deviceDataRepository->findLastRecordForDeviceId($deviceId, $entry);
+
+        return $this->data[$key];
+    }
+}
