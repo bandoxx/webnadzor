140e1dcbd870870e7e7eef73096d192d00f2c3ab
Radivoje Pupovac <radivoje.pupovac98@gmail.com>
2024-07-02 12:16:26 +0200

    server date glitch

--- src/Factory/DeviceDataArchiveFactory.php
+++ src/Factory/DeviceDataArchiveFactory.php
@@ -11,6 +11,7 @@ class DeviceDataArchiveFactory
     public function create(Device $device, \DateTime $serverDate, int $entry, string $fileName, string $period): DeviceDataArchive
     {
         $archive = new DeviceDataArchive();
+        $serverDate = (clone $serverDate)->modify('+1 day')->setTime(0, 15);
 
         $archive->setDevice($device)
             ->setServerDate(new \DateTime())
