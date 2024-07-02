31778feeb36733eb4ea788b40246e2b90362d3e5
Radivoje Pupovac <radivoje.pupovac98@gmail.com>
2024-07-02 13:44:29 +0200

    Monthly fix

--- src/Controller/DeviceArchive/API/DeviceDataMonthlyArchiveController.php
+++ src/Controller/DeviceArchive/API/DeviceDataMonthlyArchiveController.php
@@ -7,12 +7,13 @@ use App\Repository\DeviceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\RouterInterface;
 
 #[Route(path: '/api/device/{id}/{entry}/archive/monthly', name: 'app_api_devicedataarchive_getmonthlydata', methods: 'GET')]
 class DeviceDataMonthlyArchiveController extends AbstractController
 {
 
-    public function __invoke(int $id, int $entry, DeviceRepository $deviceRepository, DeviceDataArchiveRepository $deviceDataArchiveRepository): Response
+    public function __invoke(int $id, int $entry, DeviceRepository $deviceRepository, DeviceDataArchiveRepository $deviceDataArchiveRepository, RouterInterface $router): Response
     {
         $device = $deviceRepository->find($id);
         $archiveData = $deviceDataArchiveRepository->getMonthlyArchives($device, $entry);
@@ -23,8 +24,18 @@ class DeviceDataMonthlyArchiveController extends AbstractController
                 ++$i,
                 $data->getArchiveDate()->format('m.Y.'),
                 $data->getServerDate()->format('d.m.Y. H:i:s'),
-                sprintf('<div style="height: 3px;">&nbsp;</div><div class="actionbar"><a href="%s.xlsx" class="action view"><span>Excel</span></a></div>', $data->getFilename()),
-                sprintf('<div style="height: 3px;">&nbsp;</div><div class="actionbar"><a href="%s.pdf" class="action view"><span>PDF</span></a></div>', $data->getFilename())
+                sprintf('<div style="height: 3px;">&nbsp;</div><div class="actionbar"><a href="%s" class="action view"><span>Excel</span></a></div>',
+                    $router->generate('api_device_data_archive_download', [
+                        'id' => $data->getId(),
+                        'type' => 'xlsx'
+                    ])
+                ),
+                sprintf('<div style="height: 3px;">&nbsp;</div><div class="actionbar"><a href="%s" class="action view"><span>PDF</span></a></div>',
+                    $router->generate('api_device_data_archive_download', [
+                        'id' => $data->getId(),
+                        'type' => 'pdf'
+                    ])
+                )
             ];
         }
 
