bf597fd9c36d14a53fa8fc2f06c60991936e20e9
Radivoje Pupovac <radivoje.pupovac98@gmail.com>
2024-08-08 19:01:44 +0200

    Client overview

--- src/Controller/Client/OverviewController.php
+++ src/Controller/Client/OverviewController.php
@@ -2,17 +2,26 @@
 
 namespace App\Controller\Client;
 
+use App\Repository\ClientFtpRepository;
 use App\Repository\ClientRepository;
+use App\Repository\ClientSettingRepository;
 use App\Service\DeviceLocationHandler;
 use App\Service\PermissionChecker;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Exception\BadRequestException;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route(path: '/admin/{clientId}/overview', name: 'client_overview')]
 class OverviewController extends AbstractController
 {
-    public function __invoke(int $clientId, ClientRepository $clientRepository, DeviceLocationHandler $deviceLocationHandler): Response
+    public function __invoke(
+        int $clientId,
+        ClientRepository $clientRepository,
+        DeviceLocationHandler $deviceLocationHandler,
+        ClientSettingRepository $clientSettingRepository,
+        ClientFtpRepository $clientFtpRepository
+    ): Response
     {
         $client = $clientRepository->find($clientId);
 
@@ -20,8 +29,10 @@ class OverviewController extends AbstractController
             throw $this->createAccessDeniedException();
         }
 
-        return $this->render('v1/overview/user.html.twig', [
+        return $this->render('v2/overview/user.html.twig', [
             'devices_table' => $deviceLocationHandler->getClientDeviceLocationData($this->getUser(), $client),
+            'settings' => $clientSettingRepository->findOneBy(['client' => $clientId]),
+            'ftp' => $clientFtpRepository->findOneBy(['client' => $clientId])
         ]);
     }
 
