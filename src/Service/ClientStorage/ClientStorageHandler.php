5385b0a14a8b492beb816b87dadab75f8f09c8ae
Radivoje Pupovac <radivoje.pupovac98@gmail.com>
2024-08-31 17:08:59 +0200

    rh/t names in dropdown

--- src/Service/ClientStorage/ClientStorageHandler.php
+++ src/Service/ClientStorage/ClientStorageHandler.php
@@ -65,18 +65,18 @@ class ClientStorageHandler
 
         foreach ($devices as $device) {
             for ($entry = 1; $entry <= 2; $entry++) {
-                $text = sprintf("%s, %s, %s", $device->getName(), $device->getEntryData($entry)['t_location'], $device->getEntryData($entry)['t_name']);
+                $text = sprintf("%s, %s", $device->getName(), $device->getEntryData($entry)['t_location']);
                 if ($device->isTUsed($entry)) {
                     $list[] = [
                         'value' => sprintf("%s-%s-t", $device->getId(), $entry),
-                        'text' => sprintf("%s - Temperatura", $text),
+                        'text' => sprintf("%s, %s - Temperatura", $text,  $device->getEntryData($entry)['t_name']),
                     ];
                 }
 
                 if ($device->isRhUsed($entry)) {
                     $list[] = [
                         'value' => sprintf("%s-%s-rh", $device->getId(), $entry),
-                        'text' => sprintf("%s - Vlaga", $text),
+                        'text' => sprintf("%s, %s - Vlaga", $text,  $device->getEntryData($entry)['rh_name']),
                     ];
                 }
             }
