2336d3779e2480a5d2a7fbcdf7f84c9857db1ca9
Radivoje Pupovac <radivoje.pupovac98@gmail.com>
2024-09-19 16:04:46 +0200

    device icon fix

--- src/Factory/DeviceIconFactory.php
+++ src/Factory/DeviceIconFactory.php
@@ -7,7 +7,7 @@ use App\Entity\DeviceIcon;
 class DeviceIconFactory
 {
 
-    public function create(string $title, string $fileName): DeviceIcon
+    public function create(string $fileName, string $title): DeviceIcon
     {
         return (new DeviceIcon())
             ->setTitle($title)
