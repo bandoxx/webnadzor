4e0926f56d7e07d0521f18d8ea0896de43574695
banox <rasho@banox.dev>
2025-10-03 14:15:15 +0200

    Added logger for mails

--- src/Service/Mailer.php
+++ src/Service/Mailer.php
@@ -3,13 +3,16 @@
 namespace App\Service;
 
 use App\Repository\SmtpRepository;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Mailer\Transport;
 use Symfony\Component\Mime\RawMessage;
 
 class Mailer
 {
-    public function __construct(private SmtpRepository $smtpRepository)
-    {
+    public function __construct(
+        private SmtpRepository $smtpRepository,
+        private LoggerInterface $logger,
+    ) {
     }
 
     public function send(RawMessage $message): void
@@ -17,19 +20,24 @@ class Mailer
         $smtp = $this->smtpRepository->findOneBy([]);
 
         if (!$smtp) {
+            $this->logger->warning('Mailer: SMTP configuration not found, skipping send.', ['channel' => 'mailer']);
             return;
         }
 
-        $dns = sprintf('smtp://%s:%s@%s:%s',
+        $dsn = sprintf('smtp://%s:%s@%s:%s',
             urlencode($smtp->getUsername()),
             urlencode($smtp->getPassword()),
             urlencode($smtp->getHost()),
             $smtp->getPort()
         );
 
-        $transport = Transport::fromDsn($dns);
+        $this->logger->debug('Mailer: Preparing to send email via DSN.', ['dsn' => $dsn]);
+
+        $transport = Transport::fromDsn($dsn);
         $mailer = new \Symfony\Component\Mailer\Mailer($transport);
 
         $mailer->send($message);
+
+        $this->logger->info('Mailer: Email sent successfully.');
     }
 }
