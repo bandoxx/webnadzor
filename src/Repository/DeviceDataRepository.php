0b3e753ca127bd7e6e1712475f2634385f00e729
banox <radivoje.pupovac98@gmail.com>
2026-01-12 11:47:55 +0100

    fix: Ignoring seconds in preview/insert shift data

--- src/Repository/DeviceDataRepository.php
+++ src/Repository/DeviceDataRepository.php
@@ -364,7 +364,8 @@ class DeviceDataRepository extends ServiceEntityRepository
                     SELECT 1
                     FROM device_data dd2
                     WHERE dd2.device_id = dd.device_id
-                      AND dd2.device_date = DATE_ADD(dd.device_date, INTERVAL :intervalDays DAY)
+                      AND dd2.device_date >= DATE_ADD(dd.device_date, INTERVAL :intervalDays DAY) - INTERVAL SECOND(DATE_ADD(dd.device_date, INTERVAL :intervalDays DAY)) SECOND
+                      AND dd2.device_date <= DATE_ADD(dd.device_date, INTERVAL :intervalDays DAY) - INTERVAL SECOND(DATE_ADD(dd.device_date, INTERVAL :intervalDays DAY)) SECOND + INTERVAL 59 SECOND
                       AND NOT ((dd2.t1 IS NULL OR dd2.t1 = 0) AND (dd2.t2 IS NULL OR dd2.t2 = 0))
                 )
         ';
@@ -434,7 +435,8 @@ class DeviceDataRepository extends ServiceEntityRepository
                     SELECT 1
                     FROM device_data dd2
                     WHERE dd2.device_id = dd.device_id
-                      AND dd2.device_date = DATE_ADD(dd.device_date, INTERVAL :intervalDays DAY)
+                      AND dd2.device_date >= DATE_ADD(dd.device_date, INTERVAL :intervalDays DAY) - INTERVAL SECOND(DATE_ADD(dd.device_date, INTERVAL :intervalDays DAY)) SECOND
+                      AND dd2.device_date <= DATE_ADD(dd.device_date, INTERVAL :intervalDays DAY) - INTERVAL SECOND(DATE_ADD(dd.device_date, INTERVAL :intervalDays DAY)) SECOND + INTERVAL 59 SECOND
                       AND NOT ((dd2.t1 IS NULL OR dd2.t1 = 0) AND (dd2.t2 IS NULL OR dd2.t2 = 0))
                 )
             ORDER BY dd.device_date
@@ -527,7 +529,8 @@ class DeviceDataRepository extends ServiceEntityRepository
                     SELECT 1
                     FROM device_data dd2
                     WHERE dd2.device_id = dd.device_id
-                      AND dd2.device_date = DATE_ADD(dd.device_date, INTERVAL :intervalDays DAY)
+                      AND dd2.device_date >= DATE_ADD(dd.device_date, INTERVAL :intervalDays DAY) - INTERVAL SECOND(DATE_ADD(dd.device_date, INTERVAL :intervalDays DAY)) SECOND
+                      AND dd2.device_date <= DATE_ADD(dd.device_date, INTERVAL :intervalDays DAY) - INTERVAL SECOND(DATE_ADD(dd.device_date, INTERVAL :intervalDays DAY)) SECOND + INTERVAL 59 SECOND
                       AND NOT ((dd2.t1 IS NULL OR dd2.t1 = 0) AND (dd2.t2 IS NULL OR dd2.t2 = 0))
                 )
             ORDER BY dd.device_date
